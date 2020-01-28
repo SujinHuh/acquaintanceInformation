de092cffba282d137bc33a71f110caf250c50978
SujinHuh <juin483@naver.com>
2020-01-28 22:06:36 +0900

    * Lombok
       - @Data

--- src/main/java/me/acquaintanceinformation/mycontact/domain/Person.java
+++ src/main/java/me/acquaintanceinformation/mycontact/domain/Person.java
@@ -8,14 +8,10 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
-@Getter
-@Setter
-@ToString
 @NoArgsConstructor// 인자가 없는 생성자
 @AllArgsConstructor //생성할때 인자값이 있는 생성자
 @RequiredArgsConstructor //    @GeneratedValue 자동으로 번호가 생성되니 값을 빼고 생성
-@EqualsAndHashCode
-
+@Data
 public class Person {
 
     @Id
